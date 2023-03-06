1366ba71fe451431b89e3eac9ce3df4725cba5db
Kemal <outsider.pld@gmail.com>
2023-03-06 23:38:41 +0000

    added Products and List component plus styling

---
+++ client/src/components/List/List.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Card } from "../Card/Card";
+import "./List.scss";
+
+const List = () => {
+  const data = [{}];
+
+  return (
+    <div className="list">
+      {data?.map((item) => (
+        <Card item={item} key={item.id} />
+      ))}
+    </div>
+  );
+};
+
+export default List;
